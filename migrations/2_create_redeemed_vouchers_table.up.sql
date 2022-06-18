a67f564e2bafeeaf039dec15cc13af2b91334ae0
Sina Haseli <s.haseli@maxcapital.ir>
2022-06-18 18:23:11 +0430

    delete step in migration

--- migrations/2_create_redeemed_vouchers_table.up.sql
+++ migrations/2_create_redeemed_vouchers_table.up.sql
@@ -3,7 +3,6 @@ CREATE TABLE redeemed_voucher
     id         serial    not null,
     user_id    INT       NOT NULL,
     voucher_id INT       NOT NULL,
-    step       INT       NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT voucher_id_user_id UNIQUE (voucher_id, user_id),
     CONSTRAINT limiter_unique UNIQUE (voucher_id, step),
